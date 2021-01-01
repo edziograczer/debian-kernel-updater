8f32bcfe22027c559e80389875e761fa11f4b47a
edziograczer <edziograczer@github.com>
2021-01-01 04:18:38 +0100

    działa

---
+++ kernel-updater.py
@@ -0,0 +1,9 @@
+import os
+a = input("używasz skryptu pierwszy raz? (y/n) ")
+print("okeej")
+if a == 'y':
+    os.system('wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh')
+    os.system('sudo install ubuntu-mainline-kernel.sh /usr/local/bin/')
+    os.system('sudo ubuntu-mainline-kernel.sh -i')
+else:
+    os.system('sudo ./ubuntu-mainline-kernel.sh -i')
