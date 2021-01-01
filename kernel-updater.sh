2f1d6398898d25e72c7cb57b189e33f8006d83e0
edziograczer <edziograczer@github.com>
2021-01-01 03:39:40 +0100

    naprawione 100% working

--- kernel-updater.sh
+++ kernel-updater.sh
@@ -5,7 +5,7 @@ if[ $czy == 'y' ]
 then
 	wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 	sudo install ubuntu-mainline-kernel.sh /usr/local/bin/
-	./ubuntu-mainline-kernel.sh -i
+	sudo ubuntu-mainline-kernel.sh -i | bash
 else
-	./ubuntu-mainline-kernel.sh -i
+	sudo ./ubuntu-mainline-kernel.sh -i | bash
 fi
